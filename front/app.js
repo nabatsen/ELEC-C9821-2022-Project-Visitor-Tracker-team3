5d31c9d943e4862c4c291dc902baa755cbaf9f4b
Nhut (Nana) Cao <nhutmcao@gmail.com>
2022-05-05 09:18:19 +0300

    Update calendar

--- front/app.js
+++ front/app.js
@@ -16,12 +16,16 @@ const example_acc3 = {
 const accounts = [example_acc1, example_acc2, example_acc3];
 
 const btnLogin = document.querySelector('.login__btn');
+const calendar = document.querySelector('.container');
 
 const loginUsername = document.querySelector('.username');
 const loginPassword = document.querySelector('.password');
 
 //////////
+const showStatistic = function () {};
+
 const updateUI = function () {
+  calendar.classList.remove('hidden');
   showStatistic();
 };
 
@@ -35,3 +39,87 @@ btnLogin.addEventListener('click', function (e) {
     updateUI();
   }
 });
+
+////////////////////////// Calendar
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector('.days');
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+
+  document.querySelector('.date p').innerHTML = new Date().toDateString();
+
+  let days = '';
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+  }
+  monthDays.innerHTML = days;
+};
+
+document.querySelector('.prev').addEventListener('click', () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
